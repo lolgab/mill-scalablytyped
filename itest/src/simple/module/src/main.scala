e2165ca682091a7e4151e8c8ccf0bbe7c90cd5e4
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-01-21 17:04:52 +0100

    Fix integration test

--- itest/src/simple/module/src/main.scala
+++ itest/src/simple/module/src/main.scala
@@ -1,7 +1,8 @@
 import typings.lodash.mod.^
+import typings.std.global.console
 import scala.scalajs.js
 
 @main
 def main() =
   val res = ^.partition(js.Array(1, 2, 3, 4), n => n.asInstanceOf[Int] % 2)
-  println(res)
+  console.log(res)
