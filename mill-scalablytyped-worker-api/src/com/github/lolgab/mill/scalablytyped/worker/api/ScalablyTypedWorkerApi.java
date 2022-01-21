6a9062e07ec2004e88f29b3d184d02d3cf0aa3ff
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-01-21 20:01:05 +0100

    Add some configurations

--- mill-scalablytyped-worker-api/src/com/github/lolgab/mill/scalablytyped/worker/api/ScalablyTypedWorkerApi.java
+++ mill-scalablytyped-worker-api/src/com/github/lolgab/mill/scalablytyped/worker/api/ScalablyTypedWorkerApi.java
@@ -3,5 +3,5 @@ package com.github.lolgab.mill.scalablytyped.worker.api;
 import java.nio.file.Path;
 
 public interface ScalablyTypedWorkerApi {
-  ScalablyTypedWorkerDep[] scalablytypedImport(Path pw, Path ivyHomePath, String scalaVersion, String scalaJSVersion);
+	ScalablyTypedWorkerDep[] scalablytypedImport(Path basePath, Path ivyHomePath, Path targetPath, String scalaVersion, String scalaJSVersion, String[] ignoredLibs);
 }
